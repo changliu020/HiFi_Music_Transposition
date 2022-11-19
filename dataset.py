7f815df5630424ec3942f77ef0a87c47b3b33a6c
changliu020 <changliu020@gmail.com>
2022-11-18 23:25:32 -0800

    first push

---
+++ dataset.py
@@ -0,0 +1,17 @@
+import librosa
+import os
+from torch.utils.data.dataset import Dataset
+
+
+class NottinghamDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.name_list = os.listdir(self.data_dir)
+
+    def __len__(self):
+        return len(self.name_list)
+
+    def __getitem__(self, idx):
+        arr, sr = librosa.load(os.path.join(self.data_dir, self.name_list[idx]))
+        return arr, sr
+    
